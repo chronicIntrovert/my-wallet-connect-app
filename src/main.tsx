3840b8b5d54f78584593e80927a736b5376e27b3
Thinh Vu <thinh@bitski.com>
2023-04-17 15:22:26 -0400

    Add Bitski config

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import * as ReactDOM from 'react-dom/client'
 import { WagmiConfig } from 'wagmi'
 
 import { App } from './App'
+import bitskiLogo from './assets/bitski.svg';
 import { chains, client, walletConnectProjectId } from './wagmi'
 
 const ethereumClient = new EthereumClient(client, chains)
@@ -16,6 +17,29 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Web3Modal
         projectId={walletConnectProjectId}
         ethereumClient={ethereumClient}
+        mobileWallets={[
+          {
+            id: 'bitski',
+            name: 'Bitski',
+            links: {
+              native: 'bitski://',
+              universal: 'https://wallet.bitski.com/walletconnect',
+            }
+          }
+        ]}
+        desktopWallets={[
+          {
+            id: 'bitski',
+            name: 'Bitski',
+            links: {
+              native: 'https://chrome.google.com/webstore/detail/bitski/feejiigddaafeojfddjjlmfkabimkell',
+              universal: 'https://wallet.bitski.com',
+            }
+          }
+        ]}
+        walletImages={{
+          bitski: bitskiLogo
+        }}
       />
     </WagmiConfig>
   </React.StrictMode>,
